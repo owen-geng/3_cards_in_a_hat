3d3459cc304b58d03eb6d87dfe1dc010f963bd9f
owen-geng <owen_geng@hotmail.com>
2026-04-03 03:26:23 +0100

    comments

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ class Card():
 class Hat():
     def __init__(self):
         self.contents = []
-        self.contents.append(Card("red", "red"))
-        self.contents.append(Card("blue", "blue"))
-        self.contents.append(Card("red", "blue"))
+        self.contents.append(Card("red", "red")) #Add red + red
+        self.contents.append(Card("blue", "blue")) #Add blue + blue
+        self.contents.append(Card("red", "blue")) #Add red + blue
     
     def draw(self):
         
-        rand = random.randint(0,2)
-        face = random.randint(0,1)
+        rand = random.randint(0,2) #Choose a random card
+        face = random.randint(0,1) #Choose a random face
 
+        #Returns the seen face, and the unseen face
         return self.contents[rand].faces[face], self.contents[rand].faces[1-face]
-    
+        
 
 if __name__ == "__main__":
     
@@ -31,12 +32,12 @@ if __name__ == "__main__":
 
     for i in range(trials):
         
-        seen_face, unseen_face = hat.draw()
+        seen_face, unseen_face = hat.draw() #Draw a card
 
-        if seen_face == "red":
-            if unseen_face == "red":
+        if seen_face == "red": #If seen face is red
+            if unseen_face == "red": #If the other side is red, increment red_count by one
                 red_count += 1
-            else:
+            else: #Otherwise, increment blue_count (other side is blue)
                 blue_count += 1
     
     print(f"Number of times the other face was red: {red_count}")
